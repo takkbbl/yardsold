175c247404968eb0ab3912d50919413637ce38a4
unknown <mail@felixmelchner.de>
2020-07-18 14:32:08 +0200

    removed next-images dependency

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-const withSass = require('sass')
+const withSass = require("sass");
 module.exports = {
   /* config options here */
 };
-const withImages = require('next-images')
-
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-})
